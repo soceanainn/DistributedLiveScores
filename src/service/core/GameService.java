235fc691708eb9a183b1d7e3634208e096d2267b
briang95 <brian.gallagher.3@ucdconnect.ie>
2017-12-28 22:39:50 +0000

    removes registry package

--- src/service/core/GameService.java
+++ src/service/core/GameService.java
@@ -2,15 +2,13 @@ package service.core;
 
 import java.util.List;
 
-import service.registry.Service;
-
 /**
  * Interface to define the behaviour of a game service.
  */
-public interface GameService extends Service {
+public interface GameService {
 	public List<Event> getEvents(Game g);
 
 	public void addEvent(Game g, Event e);
-	
+
 	public Game createGame(String t1, String t2);
 }
